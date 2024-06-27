8548516b1d3a21ce36247827c323a76cefe6658f
DeepeshNST <deepesh.agrawal@newtonschool.co>
2024-06-27 12:00:19 +0530

    add host

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 
 const connection = mysql.createConnection({
-    host: process.env.SQL_DB_DATABASENAME,
+    host: process.env.SQL_DB_HOST,
     user: process.env.SQL_DB_USERNAME,
     password: process.env.SQL_DB_PASSWORD,
     database: process.env.SQL_DB_DATABASENAME
